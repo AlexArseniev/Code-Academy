af6895e5cc88423786dde56e539dbe67a207a6a1
Alexander Arseniev <76963990+AlexArseniev@users.noreply.github.com>
2021-02-19 01:07:34 +0200

    Update 20210217_18.c

--- Lecture 17/20210217_18.c
+++ Lecture 17/20210217_18.c
@@ -1,1 +1,8 @@
 
+/*
+Selection Sort    Complexity: n^2   Best for: linked lists
+Binary Sort       
+Merge Sort        Complexity: nlogn Best for: parallelization
+Radix Sort        
+Insertion Sort    Complexity: n Best for: small datasets
+*/
