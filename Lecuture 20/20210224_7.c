0da5b9ce84e5a588e627958c493862e6aa37594a
Alexander Arseniev <76963990+AlexArseniev@users.noreply.github.com>
2021-02-25 17:19:22 +0200

    Create 20210224_7.c

---
+++ Lecuture 20/20210224_7.c
@@ -0,0 +1,13 @@
+/*
+    Задача 7. Напишете заглавен файл с декларации на функции и
+    сложете гардове. Този начин намалява времето за компилиране на
+    големи проекти
+*/
+
+#ifndef MY_LIB
+    #define MY_LIB
+
+    void printString(char *sting);
+    int addNumbers(int a, int b);
+
+#endif
