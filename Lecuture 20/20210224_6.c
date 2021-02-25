b436a98d749bdf9f66b1b77ad767792bd386474f
Alexander Arseniev <76963990+AlexArseniev@users.noreply.github.com>
2021-02-25 17:19:03 +0200

    Create 20210224_6.c

---
+++ Lecuture 20/20210224_6.c
@@ -0,0 +1,14 @@
+/*
+Задача 6. Напишете макрос с променлив брой параметри, който
+извиква printf със префикс “TRACE: ”
+*/
+
+#include <stdio.h>
+#define MY_PRINT(...) printf("TRACE: " __VA_ARGS__)
+
+int main() {
+
+    MY_PRINT("My favorite numbers are: %d, %d, %d\n", 3,7,21);
+    
+    return 0;
+}
