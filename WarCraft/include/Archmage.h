8c80c7ecb460f0f75165a36b43e8e9d5328f1acb
Alexander Arseniev <76963990+AlexArseniev@users.noreply.github.com>
2021-07-19 14:54:47 +0300

    Create Archmage.h

---
+++ WarCraft/include/Archmage.h
@@ -0,0 +1,5 @@
+#include "Hero.h"
+
+void createArchmage(Hero *archmage, const char *inputName, int inputMaxMana, int inputManaRegenRate, int manaRegMod);
+void basicSpellArchmage(Hero *archmage);
+void ultimateSpellArchmage(Hero *archmage);
