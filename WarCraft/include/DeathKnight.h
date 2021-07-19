71a332e0652fc06fc662482c3ff2c2f4ab6ac485
Alexander Arseniev <76963990+AlexArseniev@users.noreply.github.com>
2021-07-19 14:55:18 +0300

    Add files via upload

---
+++ WarCraft/include/DeathKnight.h
@@ -0,0 +1,3 @@
+void createDeathKnight(Hero *deathKnight, const char *inputName, int inputMaxMana, int inputManaRegenRate);
+void basicSpellDeathKnight(Hero *deathKnight);
+void ultimateSpellDeathKinght(Hero *deathKnight);
---
+++ WarCraft/include/DrawRanger.h
@@ -0,0 +1,3 @@
+void createDrawRanger(Hero *drawRanger, const char *inputName, int inputMaxMana, int inputManaRegenRate);
+void basicSpellDrawRanger(Hero *drawRanger);
+void ultimateSpellDrawRanger(Hero *drawRanger);
